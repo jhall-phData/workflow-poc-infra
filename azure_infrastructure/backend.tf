181158a185b866d36f95f530f45fb3d02f8fa70f
Joe Hall <jhall@phdata.io>
2022-01-12 16:14:34 -0500

    add resource group to backend

--- azure_infrastructure/backend.tf
+++ azure_infrastructure/backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    resource_group_name  = "DefaultResourceGroup-CUS"
     storage_account_name = "workflowpoc"
     container_name       = "tf-state"
     key                  = "workflow-poc-infra.tfstate"
