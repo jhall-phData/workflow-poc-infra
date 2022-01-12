f386e7edd7c268f97c89c97cbac6867c6fa50204
Joe Hall <jhall@phdata.io>
2022-01-12 17:08:16 -0500

    add sub to backend

--- azure_infrastructure/backend.tf
+++ azure_infrastructure/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "azurerm" {
+    subscription_id      = "858549e2-8aab-4d4f-ad8d-d87e01785d46"
     resource_group_name  = "DefaultResourceGroup-CUS"
     storage_account_name = "workflowpoc"
     container_name       = "tf-state"
     key                  = "workflow-poc-infra.tfstate"
-    }
+  }
 }
