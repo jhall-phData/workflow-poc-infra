fa669f448101d13df2bef4d79c903b808d9ac990
Joe Hall <jhall@phdata.io>
2022-01-12 20:48:22 -0500

    update provider version

--- azure_infrastructure/main.tf
+++ azure_infrastructure/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "> 2.31.1"
+  version = "2.91.0"
   features {}
   subscription_id = "858549e2-8aab-4d4f-ad8d-d87e01785d46"
   client_id       = var.workflow-poc-sp-client-id
