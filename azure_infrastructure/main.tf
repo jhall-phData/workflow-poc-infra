4afe2994f2fa244be1b2e19099d477f77a89ea5d
Joe Hall <jhall@phdata.io>
2022-01-12 20:41:47 -0500

    x

--- azure_infrastructure/main.tf
+++ azure_infrastructure/main.tf
@@ -1,13 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.56.0"
-    }
-  }
-}
-
 provider "azurerm" {
+  version = "> 2.31.1"
   features {}
   subscription_id = "858549e2-8aab-4d4f-ad8d-d87e01785d46"
   client_id       = var.workflow-poc-sp-client-id
