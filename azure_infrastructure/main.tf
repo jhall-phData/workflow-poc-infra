d888011ffaee86e1f7fef2d99084f8d39fb28756
Joe Hall <jhall@phdata.io>
2022-01-12 20:35:32 -0500

    add providers to main

--- azure_infrastructure/main.tf
+++ azure_infrastructure/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.56.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "858549e2-8aab-4d4f-ad8d-d87e01785d46"
