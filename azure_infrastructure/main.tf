33ef187d74c6301f2c07bb27f4da1f30b0d5fe22
Joe Hall <jhall@phdata.io>
2022-01-12 20:50:11 -0500

    update main

--- azure_infrastructure/main.tf
+++ azure_infrastructure/main.tf
@@ -1,8 +1,18 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  version = "2.91.0"
   features {}
   subscription_id = "858549e2-8aab-4d4f-ad8d-d87e01785d46"
   client_id       = var.workflow-poc-sp-client-id
   client_secret   = var.workflow-poc-sp-secret
   tenant_id       = var.workflow-poc-sp-tenant-id
 }
+
