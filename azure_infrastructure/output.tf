e3ce261a7f2db8a39ec4059f644f5c56606e5e02
Joe Hall <jhall@phdata.io>
2022-01-16 22:17:35 -0500

    no more aks

--- azure_infrastructure/output.tf
+++ azure_infrastructure/output.tf
@@ -1,16 +1,16 @@
 
-output "id" {
-  value = azurerm_kubernetes_cluster.eqrdp.id
-}
+# output "id" {
+#   value = azurerm_kubernetes_cluster.eqrdp.id
+# }
 
-output "kube_config" {
-  sensitive = true
-  value     = azurerm_kubernetes_cluster.eqrdp.kube_config_raw
-}
+# output "kube_config" {
+#   sensitive = true
+#   value     = azurerm_kubernetes_cluster.eqrdp.kube_config_raw
+# }
 
-output "client_key" {
-  value = azurerm_kubernetes_cluster.eqrdp.kube_config.0.client_key
-}
+# output "client_key" {
+#   value = azurerm_kubernetes_cluster.eqrdp.kube_config.0.client_key
+# }
 
 #output "client_certificate" {
 #  value = azurerm_kubernetes_cluster.eqrdp.kube_config.0.client_certificate
@@ -20,12 +20,12 @@ output "client_key" {
 #   value = azurerm_kubernetes_cluster.eqrdp.kube_config.0.cluster_ca_certificate
 # }
 
-output "host" {
-  value = azurerm_kubernetes_cluster.eqrdp.kube_config.0.host
-}
+# output "host" {
+#   value = azurerm_kubernetes_cluster.eqrdp.kube_config.0.host
+# }
 
-output "admin_password" {
-  sensitive = true
-  value       = azurerm_container_registry.acr.admin_password
-  description = "The object ID of the user"
-}
+# output "admin_password" {
+#   sensitive = true
+#   value       = azurerm_container_registry.acr.admin_password
+#   description = "The object ID of the user"
+# }
