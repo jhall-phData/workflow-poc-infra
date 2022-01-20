dac178706f59279187f8022829232899849b038f
Joe Hall <jhall@phdata.io>
2022-01-20 13:35:53 -0500

    typo

--- azure_infrastructure/backend.tf
+++ azure_infrastructure/backend.tf
@@ -3,7 +3,7 @@ terraform {
     subscription_id      = "858549e2-8aab-4d4f-ad8d-d87e01785d46"
     resource_group_name  = "DefaultResourceGroup-CUS"
     storage_account_name = "workflowpoc"
-    container_name       = "tf-stateQA"
+    container_name       = "tf-stateqa"
     key                  = "workflow-poc-infra.tfstate"
   }
 }
