a7fe2c8388b61b8e1dda319091cec547ca99751d
Joe Hall <jhall@phdata.io>
2022-01-20 13:31:36 -0500

    quick change for QA

--- azure_infrastructure/backend.tf
+++ azure_infrastructure/backend.tf
@@ -3,7 +3,7 @@ terraform {
     subscription_id      = "858549e2-8aab-4d4f-ad8d-d87e01785d46"
     resource_group_name  = "DefaultResourceGroup-CUS"
     storage_account_name = "workflowpoc"
-    container_name       = "tf-state"
+    container_name       = "tf-stateQA"
     key                  = "workflow-poc-infra.tfstate"
   }
 }
